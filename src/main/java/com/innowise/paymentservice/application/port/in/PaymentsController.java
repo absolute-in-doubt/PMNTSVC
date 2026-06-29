c601e146f0b0f9bc28e93ee10f722b6b170223b2
AbsoluteInDoubt <pervysbor@gmail.com>
2026-06-29 20:03:22 +0300

    chore: added PaymentService interface, replaced UserCOntext with more common Authentication for role check in the PaymentsController

--- src/main/java/com/innowise/paymentservice/application/port/in/PaymentsController.java
+++ src/main/java/com/innowise/paymentservice/application/port/in/PaymentsController.java
@@ -1,21 +1,21 @@
 package com.innowise.paymentservice.application.port.in;
 
 import com.innowise.paymentservice.application.dto.*;
-import com.innowise.paymentservice.application.security.model.UserContext;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 
 public interface PaymentsController {
 
-    ResponseEntity<PaymentResponseDto> initiatePayment(UserContext userContext, CreatePaymentRequestDto requestDto);
+    ResponseEntity<PaymentResponseDto> initiatePayment(Authentication authentication, CreatePaymentRequestDto requestDto);
 
-    ResponseEntity<PaymentResponseDto> getPaymentById(UserContext userContext, String paymentId);
+    ResponseEntity<PaymentResponseDto> getPaymentById(Authentication authentication, String paymentId);
 
-    ResponseEntity<Page<PaymentResponseDto>> getPaymentsFiltered(UserContext userContext, PaymentFilter paymentFilter);
+    ResponseEntity<Page<PaymentResponseDto>> getPaymentsFiltered(Authentication authentication, PaymentFilter paymentFilter);
 
-    ResponseEntity<PaymentsSummaryResponseDto> getPaymentSummaryForAuthenticatedUser(UserContext userContext, PaymentSummaryFilter psFilter);
+    ResponseEntity<PaymentsSummaryResponseDto> getPaymentSummaryForAuthenticatedUser(Authentication authentication, PaymentSummaryFilter psFilter);
 
-    ResponseEntity<PaymentsSummaryResponseDto> getPaymentSummaryForAdmin(UserContext userContext, PaymentSummaryFilter psFilter);
+    ResponseEntity<PaymentsSummaryResponseDto> getPaymentSummaryForAdmin(Authentication authentication, PaymentSummaryFilter psFilter);
 
     ResponseEntity<PaymentsSummaryResponseDto> getPaymentSummaryForAll(PaymentSummaryFilter psFilter);
 }
---
+++ src/main/java/com/innowise/paymentservice/application/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.innowise.paymentservice.application.service;
+
+import com.innowise.paymentservice.application.dto.*;
+import org.springframework.data.domain.Page;
+
+public interface PaymentService {
+
+    PaymentResponseDto initiatePayment(Long userId, CreatePaymentRequestDto requestDto);
+
+    PaymentResponseDto getPaymentById(Long userId, String paymentId, boolean isAdmin);
+
+    //All the user role checks should be performed in the controller
+    Page<PaymentResponseDto> getPaymentsFiltered(PaymentFilter paymentFilter);
+
+    PaymentsSummaryResponseDto getPaymentSummary(PaymentSummaryFilter psFilter);
+
+}
