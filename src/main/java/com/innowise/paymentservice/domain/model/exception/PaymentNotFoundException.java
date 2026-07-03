b625f3c35be484b090626fc646727db2ac532226
AbsoluteInDoubt <pervysbor@gmail.com>
2026-07-03 18:53:27 +0300

    fix: fixed PaymentNotFoundException

--- src/main/java/com/innowise/paymentservice/domain/model/exception/PaymentNotFoundException.java
+++ src/main/java/com/innowise/paymentservice/domain/model/exception/PaymentNotFoundException.java
@@ -2,6 +2,6 @@ package com.innowise.paymentservice.domain.model.exception;
 
 public class PaymentNotFoundException extends RuntimeException {
     public PaymentNotFoundException(String message) {
-        super( + paymentId);
+        super(message);
     }
 }
